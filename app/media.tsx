c7e45438a799b36346fef54c64fda5f586b1a912
Ammar Iqbal <ammariqbal043@gmail.com>
2024-08-14 10:51:30 +0500

    implement `react-native-pager-view`

--- app/media.tsx
+++ app/media.tsx
@@ -1,46 +1,54 @@
-import { CameraRoll } from "@react-native-camera-roll/camera-roll"
-import { StatusBar } from "expo-status-bar"
+import {
+  CameraRoll,
+  PhotoIdentifier,
+} from "@react-native-camera-roll/camera-roll"
 import { useEffect, useState } from "react"
-import { FlatList, Image, View } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { Image, View } from "react-native"
+import PagerView from "react-native-pager-view"
 import tw from "twrnc"
 
 export default function Media() {
-  const [photos, setPhotos] = useState<string[]>([])
+  const [photos, setPhotos] = useState<PhotoIdentifier["node"][]>([])
+  const [hasNextPage, setHasNextPage] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
+  const [endCursor, setEndCursor] = useState<string>()
+
+  const getPhotos = async () => {
+    if (!hasNextPage || isLoading) return
+
+    setIsLoading(true)
+
+    const data = await CameraRoll.getPhotos({
+      first: 10,
+      after: endCursor,
+      groupTypes: "SavedPhotos",
+    })
+
+    setPhotos((prev) => [...prev, ...data.edges.map(({ node }) => node)])
+    setHasNextPage(data.page_info.has_next_page)
+
+    setIsLoading(false)
+    setEndCursor(data.page_info.end_cursor)
+  }
 
   useEffect(() => {
-    async function getPhotos() {
-      const photos = await CameraRoll.getPhotos({
-        first: 20,
-        groupTypes: "SavedPhotos",
-      })
-
-      setPhotos(photos.edges.map((o) => o.node.image.uri))
-    }
-
     getPhotos()
   }, [])
 
   return (
-    <>
-      <SafeAreaView style={tw`h-full mx-auto`}>
-        <FlatList
-          data={photos}
-          renderItem={Item}
-          numColumns={2}
-          keyExtractor={(item) => item}
-          style={tw`pl-2 pt-2`}
-        />
-      </SafeAreaView>
-      <StatusBar backgroundColor="black" style="light" />
-    </>
-  )
-}
-
-const Item = ({ item }: { item: string }) => {
-  return (
-    <View style={tw`max-w-[50%] pr-2 pb-2`}>
-      <Image style={tw`w-full aspect-square bg-green-500`} src={item} />
+    <View style={tw`grow`}>
+      <PagerView
+        style={tw`grow`}
+        initialPage={0}
+        onPageSelected={(e) => {
+          const newPage = e.nativeEvent.position
+          if (newPage === photos.length - 3) getPhotos()
+        }}
+      >
+        {photos.map(({ id, image }) => (
+          <Image key={id} source={image} />
+        ))}
+      </PagerView>
     </View>
   )
 }
